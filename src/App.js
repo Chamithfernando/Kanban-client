989080cf91659f32eab81d79bbe75edbc07fb0f4
Chamith <chameeefdo@gmail.com>
2021-03-05 18:52:50 +0530

    lecture7 completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Dashboard from './component/Dashboard';
+import Header from './component/Layout/Header';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 function App() {
   return (
 
     <div className="app">
-    <Dashboard/>
+      <Header/>
+      <Dashboard/>
     
+   
     </div>
 
    
     
+    
+
+
+
   );
 }
 
--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import ProjectIteam from './Project/ProjectIteam';
 class Dashboard extends Component {
     state = {  }
     render() { 
         return ( 
-
-            <div>
-            <h1>Welcome Dashboard</h1>
+            <div className="projects">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1 className="display-4 text-center">Projects</h1>
+                        <br />
+                        <a href="ProjectForm.html" className="btn btn-lg btn-info">
+                            Create a Project
+                        </a>
+                        <br />
+                        <hr />
+    
+                        <ProjectIteam/>
+                        
+                    </div>
+                </div>
             </div>
+        </div>
 
          );
     }
